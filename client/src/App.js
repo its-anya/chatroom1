0646abf1b8d3b7f0fd919b31f387c276f3858dd8
k.shanmukh <kodalishannu666@gmail.com>
2025-06-21 15:04:27 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ChatRoom from './pages/ChatRoom';
 import AdminDashboard from './pages/AdminDashboard';
+
 
 function App() {
   return (

3a9211da3a32b043e1c17e7ed0571bb90f80e1a9
k.shanmukh <kodalishannu666@gmail.com>
2025-06-21 13:42:11 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true, // Ensure usernames are unique
+    unique: true,
     trim: true
   },
   password: {

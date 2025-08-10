e87ecb265d8fbea5c8f7b958e923d906760f0c3b
k.shanmukh <kodalishannu666@gmail.com>
2025-08-10 11:19:39 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/chatroom" element={<ChatRoom />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
